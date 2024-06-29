9163076281823d4bf9bc902efedc34f5e9b907ce
Dylan Sapienza <loldsap5131@gmail.com>
2024-06-29 12:33:00 -0400

    Fix lazy

--- lua/dsapienza/config/lazy.lua
+++ lua/dsapienza/config/lazy.lua
@@ -15,9 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-        -- import your plugins
-        { import = "plugins" },
-    },
+    spec = "dsapienza.plugins",
     checker = { enabled = true },
 })

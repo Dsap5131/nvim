358b412f9adece0fc160093ff4cb679252a02d1e
Dylan Sapienza <loldsap5131@gmail.com>
2024-06-30 01:04:17 -0400

    Fix treesitter

--- lua/dsapienza/plugins/treesitter.lua
+++ lua/dsapienza/plugins/treesitter.lua
@@ -2,14 +2,18 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        opts = {
-            highlight = { enable = true },
-            indent = { enable = true },
-            ensure_installed = {
-                "python",
-                "lua",
-                "markdown",
-            }
-        }
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "python",
+                    "lua",
+                },
+                sync_install = false,
+                auto_install = true,
+                indent = { enable = true },
+                highlight = { enable = true },
+            })            
+        end
     }
 }

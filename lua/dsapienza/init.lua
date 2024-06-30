da00a4a64c5bf29436da24046ddd3930ac4f50cb
Dylan Sapienza <loldsap5131@gmail.com>
2024-06-29 22:25:02 -0400

    miscellaneous stuff

--- lua/dsapienza/init.lua
+++ lua/dsapienza/init.lua
@@ -1,3 +1,18 @@
 require("dsapienza.set")
 require("dsapienza.remap")
 require("dsapienza.config.lazy")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlighYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end
+})

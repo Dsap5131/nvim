0dae02a73e0174bb9b4653263c2075cd20e5f3bb
Dylan Sapienza <loldsap5131@gmail.com>
2024-07-01 23:45:25 -0400

    bleh lsp

--- lua/dsapienza/plugins/lsp.lua
+++ lua/dsapienza/plugins/lsp.lua
@@ -4,9 +4,16 @@ return {
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
-    
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
         },
         config = function()
+            local cmp = require('cmp')
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local 
             require("mason").setup()
             require("mason-lspconfig").setup({
                 ensure_installed = {
@@ -19,6 +26,6 @@ return {
                     end,
                 },
             })
-        end,  
+        end,
     }
 }

1ba635608cee1c738a33b3b54c814f09b1cf7274
Dylan Sapienza <loldsap5131@gmail.com>
2026-03-05 20:05:19 -0500

    fix dbt

--- lua/plugins/dbt.lua
+++ lua/plugins/dbt.lua
@@ -1,3 +1,13 @@
 return {
-    { "3fonov/dbt-nvim" }
+    "3fonov/dbt-nvim",
+    config=true,
+    keys = {
+        {'<leader>dc', '<cmd>DbtCompile<CR>', desc = 'dbt compile' },
+        {'<leader>dy', '<cmd>DbtModelYaml<CR>', desc = 'dbt model yaml' },
+        {'<leader>dr', '<cmd>DbtRun<CR>', desc = 'dbt run' },
+        {'<leader>df', '<cmd>DbtRunFull<CR>', desc = 'dbt run full' },
+        {'<leader>dt', '<cmd>DbtTest<CR>', desc = 'dbt test' },
+        {'<leader>d+', '<cmd>DbtListUpstreamModels<CR>',desc='dbt upstream'},
+        {'<leader>d-', '<cmd>DbtListDownstreamModels<CR>',desc='dbt downstream'}
+    }
 }
